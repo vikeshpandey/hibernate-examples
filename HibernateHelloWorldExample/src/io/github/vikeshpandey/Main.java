5b359c0fcad9b0489d25c5df65dbc9a987595ae2
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-04-26 03:19:17 +0200

    helloworld

---
+++ HibernateHelloWorldExample/src/io/github/vikeshpandey/Main.java
@@ -0,0 +1,16 @@
+package io.github.vikeshpandey;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+	public static void main(String[] args) {
+		StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml")
+				.build();
+		Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+		SessionFactory sessionFactory = metaData.getSessionFactoryBuilder().build();
+	}
+}
